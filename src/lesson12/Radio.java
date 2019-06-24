5e039db3e5c82e40da734bab6ff4463742798699
MirekTuruk <miroslaw.turuk@gmail.com>
2019-06-24 13:05:32 +0200

    firstMessage

---
+++ src/lesson12/Radio.java
@@ -0,0 +1,14 @@
+package lesson12;
+
+public class Radio {
+    private String modelName;
+    private long serialNumber;
+    private float inches;
+
+    public Radio(String modelName, long serialNumber, float inches) {
+        this.modelName = modelName;
+        this.serialNumber = serialNumber;
+        this.inches = inches;
+    }
+}
+}
